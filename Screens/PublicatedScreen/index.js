a7768a420b2e7ad8b7417671e51f0c11a9a2e679
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2023-02-27 20:37:36 -0500

    fix: Delete button cambiar in PublicatedScreen

--- Screens/PublicatedScreen/index.js
+++ Screens/PublicatedScreen/index.js
@@ -1,21 +1,15 @@
 import {FlatList, View, Text, Button} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {selectPublications} from '../../Store/publication/slice';
 
-// changeStatePublication
 import styles from './styles';
 
 const renderPublicationItem = ({item: {id, title, description, state}}) => {
-  // const dispatch = useDispatch();
-
   return (
     <View style={styles.listItem}>
       <View>
         <Text>{title}</Text>
         <Text>{description}</Text>
-        {state && <Text style={styles.publicated}>Publicado</Text>}
-        {!state && <Text style={styles.unpublicated}> No Publicado</Text>}
-        <Button title="Cambiar" onPress={() => {}}></Button>
       </View>
     </View>
   );
